0ab99e904742b432251f08f490e2b53266ad9208
danikingrd <danikingrd@gmail.com>
2023-09-19 14:42:43 -0400

    feat: upload new code:

--- binary-tree/main.cpp
+++ binary-tree/main.cpp
@@ -13,297 +13,269 @@
 
 #include <iostream>
 
+#ifdef _WIN32 // Check if the code is running on a Windows system
+const std::string CLEAR_COMMAND = "cls"; // Windows clear screen command
+#else
+const std::string CLEAR_COMMAND = "clear"; // UNIX/Linux/Mac clear screen command
+#endif
 
-using namespace std;
+struct Node {
+    int value;
+    Node* parent;
+    Node* left;
+    Node* right;
 
-struct Node { 
-	int value;
-	Node* parent;
-	Node* left;
-	Node* right;
-	Node(Node* parent, int value) {
-		this->parent = parent;
-		this->value = value;
-		this->left = nullptr;
-		this->right = nullptr;
-	}
+    Node(Node* parent, int value) : parent(parent), value(value), left(nullptr), right(nullptr) {}
 };
 
+class BinarySearchTree {
+private:
+    Node* root;
 
-struct BinarySearchTree {
-	Node* root;
 public:
-	BinarySearchTree() {
-		this->root = nullptr;
-}
-	void insert(int value) {
-		this->insert(this->root, NULL, value);
-	}
+    BinarySearchTree() : root(nullptr) {}
 
-	// Insertar un valor al árbol.
-	void insert(Node*& node, Node* parent, int value) {
-		if (node == nullptr) {
-			// Si el arbol está vacío, crea el nodo raíz.
-			node = new Node(parent, value);
-			return;
-		}
-		if (value < node->value) {
-			insert(node->left, parent, value);
-		} else {
-			insert(node->right, parent, value);
-		}
-	}
+    ~BinarySearchTree() {
+        destroyTree(root);
+    }
 
-	bool remove(int value) {
-		return this->remove(this->root, value);
-	}
+    void insert(int value) {
+        insert(root, nullptr, value);
+    }
 
-	bool remove(Node*& node, int value) {
-		if (node == nullptr) return false;
-		if (value < node->value) {  
-			// muevete al siguiente nodo por la izquierda.
-			remove(node->left, value);
-		} else if (value > node->value) {
-			// muevete a al siguiente nodo por la derecha.
-			remove(node->right, value);
-		} else {
-			remove(node);
-		}
-		return true;
-	}
+    bool remove(int value) {
+        return remove(root, value);
+    }
 
-	void remove(Node* node) {
-		if (node->left && node->right) {
-			// Caso 1: arbol con 2 hijos
-			// Primero buscamos el nodo derecho
-			// Luego buscanos el minimo valor a partir de ahi.
-			Node* min = BinarySearchTree::findMin(node->right);
-			node->value = min->value;
-			remove(min);
-		} else if (node->left) {
-			// Caso 2: Borrar nodo con solo 1 sub-arbol hijo
-			replaceNode(node, node->left);
-			destroyNode(node);
-		} else if (node->right) {
-			replaceNode(node, node->right);
-			destroyNode(node);
-		} else {
-			// replaceNode(node, nullptr);
-			destroyNode(node);
-		}
-	}
+    bool contains(int value) const {
+        return contains(root, value);
+    }
 
-	static void destroyNode(Node*&node) {
-		node->left = nullptr;
-		node->right = nullptr;
-		// cout << " At the moment of deleteNode address of root is " << node << endl;
-		delete node;
-		node = NULL;
-	}
-	
-	static void replaceNode(Node* target, Node* newNode) {
-		if (target->parent) {
-			if (target->value == target->parent->left->value) {
-				// Eliminar el nodo izquierdo
-				target->parent->left = newNode;
-				
-			} else if (target->value == target->parent->right->value) {
-				// Eliminar el nodo derecho
-				target->parent->right = newNode;
-			}
-		}
-		if (newNode) {
-			// asignar nuevo padre
-			newNode->parent = target->parent;
-		}
-	}
+    void display() const {
+        displayTree(root);
+    }
 
-	static Node* findMin(Node* subTree) {
-		if (!subTree) {
-			return nullptr;
-		} else if (subTree->left) {
-			return findMin(subTree->left);
-		} else {
-			return subTree;
-		}
-	}
-	
+    void printPreOrder() const {
+        printPreOrder(root);
+    }
 
+    void printInOrder() const {
+        printInOrder(root);
+    }
 
+    void printPostOrder() const {
+        printPostOrder(root);
+    }
 
-	bool contains(int value) {
-		return this->contains(this->root, value);
-	}
+    bool isEmpty() const {
+        return root == nullptr;
+    }
 
-	bool contains(Node*& node, int value) {
-		if (node == nullptr) return false;
-		if (node->value == value) {
-			return true;
-		} else if (value < node->value) {
-			return contains(node->left, value);
-		} else {
-			return contains(node->right, value);
-		}
-	}
+private:
+    void insert(Node*& node, Node* parent, int value) {
+        if (node == nullptr) {
+            node = new Node(parent, value);
+            return;
+        }
+        if (value < node->value) {
+            insert(node->left, node, value);
+        } else {
+            insert(node->right, node, value);
+        }
+    }
 
-	void display() {
-		displayTree(this->root);
-	}
+    bool remove(Node*& node, int value) {
+        if (node == nullptr) {
+            return false;
+        }
+        if (value < node->value) {
+            return remove(node->left, value);
+        } else if (value > node->value) {
+            return remove(node->right, value);
+        } else {
+            removeNode(node);
+            return true;
+        }
+    }
 
-	void displayTree(Node* node, int indent = 0) {
-		// cout << " At the moment of display address of root is " << root << endl;
-		if (node == nullptr) {
-			return;
-		}
-		displayTree(node->right, indent + 1);
-		for (int level = 0; level < indent; level++) {
-			cout << "   ";
-		}
-		
-		cout << node->value << endl;
+    void removeNode(Node*& node) {
+        if (node->left && node->right) {
+            Node* min = findMin(node->right);
+            node->value = min->value;
+            remove(node->right, min->value);
+        } else {
+            Node* temp = node;
+            if (node->left) {
+                node = node->left;
+            } else if (node->right) {
+                node = node->right;
+            } else {
+                node = nullptr;
+            }
+            delete temp;
+        }
+    }
 
-		displayTree(node->left, indent + 1);
-	}
+    Node* findMin(Node* subTree) const {
+        if (!subTree || !subTree->left) {
+            return subTree;
+        }
+        return findMin(subTree->left);
+    }
 
+    bool contains(const Node* node, int value) const {
+        if (node == nullptr) {
+            return false;
+        }
+        if (node->value == value) {
+            return true;
+        } else if (value < node->value) {
+            return contains(node->left, value);
+        } else {
+            return contains(node->right, value);
+        }
+    }
+
+    void displayTree(const Node* node, int indent = 0) const {
+        if (node == nullptr) {
+            return;
+        }
+        displayTree(node->right, indent + 1);
+        for (int level = 0; level < indent; level++) {
+            std::cout << "   ";
+        }
+        std::cout << node->value << std::endl;
+        displayTree(node->left, indent + 1);
+    }
+
+    void destroyTree(Node* node) {
+        if (node) {
+            destroyTree(node->left);
+            destroyTree(node->right);
+            delete node;
+        }
+    }
+
+    void printPreOrder(const Node* node) const {
+        if (node == nullptr) return;
+        std::cout << node->value << " - ";
+        printPreOrder(node->left);
+        printPreOrder(node->right);
+    }
+
+    void printInOrder(const Node* node) const {
+        if (node == nullptr) return;
+        printInOrder(node->left);
+        std::cout << node->value << std::endl;
+        printInOrder(node->right);
+    }
+
+    void printPostOrder(const Node* node) const {
+        if (node == nullptr) return;
+        printPostOrder(node->left);
+        printPostOrder(node->right);
+        std::cout << node->value << " - " << std::endl;
+    }
 };
 
-void printPreOrder(Node* node) {
-	if (node == nullptr) return;
-	// 1. Visit the root.
-	// 2. Traverse the left-side of the sub tree
-	// 3. Traverse the right-side of the sub tree
-	cout << node->value << " - ";
-	printPreOrder(node->left);
-	printPreOrder(node->right);
-}
+int main() {
+    std::cout << "Running Binary Search Tree program...\n";
+    BinarySearchTree tree;
+    bool exit = false;
 
-void printInOrder(Node* node) {
-	// 1. Traverse the left-side of the sub-tree
-	// 2. Visit the root.
-	// 3. Traverse the right-side of the tree.
-	if (node == nullptr) return;
-	printInOrder(node->left);
-	cout << node->value << endl;
-	printInOrder(node->right);
-}
+    while (!exit) {
+        std::cout << "Press:\n"
+                  << "   0) - Exit the program.\n"
+                  << "   1) - Insert a value into the tree.\n"
+                  << "   2) - Remove an element from the tree.\n"
+                  << "   3) - Check if the tree contains a value.\n"
+                  << "   4) - Print PreOrder traversal.\n"
+                  << "   5) - Print InOrder traversal.\n"
+                  << "   6) - Print PostOrder traversal.\n"
+                  << "   7) - Display the tree.\n"
+                  << "Enter your choice: ";
 
-void printPostOrden(Node* node) {
-	// 1. Traverse the left-side of the sub-tree
-	// 2. Traverse the right-side of the sub-tree
-	// 3. Visit the root.
-	if (node == nullptr) return;
-	printPostOrden(node->left);
-	printPostOrden(node->right);
-	cout << node->value << " - " << "\n";
-}
+        int option;
+        std::cin >> option;
 
-int readInt() {
-  	int value;
-  	while (!(cin >> value)) {
-		cout << "Ingrese un valor válido: ";
-    	cin.clear();
-    	cin.ignore(123, '\n');
-	}
-	return value;
-}
-void makeInsert(BinarySearchTree* tree) {
-	cout << "Ingrese el valor a insertar: ";
-	int data = readInt();
-	tree->insert(data);
-	printf(" * El valor %d ha sido insertado.\n", data);
-}
+        switch (option) {
+            case 0:
+                exit = true;
+                break;
+            case 1:
+                int value;
+                std::cout << "Enter the value to insert: ";
+                std::cin >> value;
+                tree.insert(value);
+                std::cout << " * Value " << value << " has been inserted.\n";
+                break;
+            case 2:
+                std::cout << "Enter the value to remove: ";
+                std::cin >> value;
+                if (tree.remove(value)) {
+                    std::cout << " * Value " << value << " has been removed.\n";
+                } else {
+                    std::cout << " * Value " << value << " was not found in the tree.\n";
+                }
+                break;
+            case 3:
+                if (tree.isEmpty()) {
+                    std::cout << " * The tree is empty. Nothing to check.\n";
+                } else {
+                    std::cout << "Enter the value to check: ";
+                    std::cin >> value;
+                    if (tree.contains(value)) {
+                        std::cout << " * Value " << value << " is in the tree.\n";
+                    } else {
+                        std::cout << " * Value " << value << " is not in the tree.\n";
+                    }
+                }
+                break;
+            case 4:
+                if (tree.isEmpty()) {
+                    std::cout << " * The tree is empty. Cannot perform traversal.\n";
+                } else {
+                    std::cout << "PreOrder Traversal: ";
+                    tree.printPreOrder();
+                    std::cout << std::endl;
+                }
+                break;
+            case 5:
+                if (tree.isEmpty()) {
+                    std::cout << " * The tree is empty. Cannot perform traversal.\n";
+                } else {
+                    std::cout << "InOrder Traversal:\n";
+                    tree.printInOrder();
+                }
+                break;
+            case 6:
+                if (tree.isEmpty()) {
+                    std::cout << " * The tree is empty. Cannot perform traversal.\n";
+                } else {
+                    std::cout << "PostOrder Traversal: ";
+                    tree.printPostOrder();
+                    std::cout << std::endl;
+                }
+                break;
+            case 7:
+                if (tree.isEmpty()) {
+                    std::cout << " * The tree is empty. Nothing to display.\n";
+                } else {
+                    std::cout << "Displaying the tree:\n";
+                    tree.display();
+                }
+                break;
+            default:
+                std::cout << " * Option " << option << " does not exist. Please try again.\n";
+                break;
+        }
 
-void checkValue(BinarySearchTree* tree) {
-	cout << "Ingrese el valor a buscar: ";
-	int data = readInt();
-	if (tree->contains(data)) {
-		printf("* El valor %d si se encuentra en el árbol\n", data);
-	} else {
-		printf(" * El valor %d no se encuentra en el árbol\n", data);
-	}
-}
-
-void makeRemove(BinarySearchTree* tree) {
-	printf("Ingrese el valor a eliminar: ");
-	int data = readInt();
-	if (tree->remove(data)) {
-		printf(" * El valor %d ha sido eliminado.\n", data);
-	} else {
-		printf(" * El valor %d no se encuentra en el árbol\n", data);
-	}
-	// tree->display();
-}
-
-
-void clear() {
-	#ifdef WINDOWS
-		std::system("cls");
-	#else
-		// assume posix
-		std::system("clear");
-	#endif
-}
-
-
-void run() {
-	BinarySearchTree* tree = new BinarySearchTree();
-	bool exit;
-	int option;
-
-	while (true) {
-		cout << "Presione: \n"
-         << "   0) - Para salir del programa.\n"
-         << "   1) - Para ingresar un valor al árbol (insert).\n"
-		 << "   2) - Para eliminar un elemento del árbol (remove)\n"
-		 << "   3) - Para verificar si el árbol contiene un valor (contains).\n"
-		 << "   4) - Para hacer un reccorido en PreOrden\n"
-		 << "   5) - Para hacer un recorrido en InOrden\n"
-		 << "   6) - Para hacer un recorrido en PostOrden\n"
-		 << "   7) - Para mostrar el árbol\n"
-         << "Ingrese su opción: ";
-		
-		option = readInt();
-
-		switch (option) {
-			case 0:
-				exit = true;
-				break;
-			case 1:
-				makeInsert(tree);
-				break;
-			case 2:
-				makeRemove(tree);
-				break;
-			case 3:
-				checkValue(tree);
-				break;
-			case 4:
-				printPreOrder(tree->root);
-				break;
-			case 5:
-				printInOrder(tree->root);
-				break;
-			case 6:
-				printPostOrden(tree->root);
-			case 7:
-				tree->display();
-				break;
-			default:
-				printf(" * La opción %d no existe. Intente de nuevo.", option);
-				break;
-		}
-		clear();	
-		tree->display();
-
-		if (exit) {
-			delete tree;
-			break;
-		}
-	}
-}
-
-int main(void) {
-	cout << "Corriendo programa árboles de búsqueda...\n";
-	run();
-	return 0;
+        if (!exit) {
+            std::cout << "Press Enter to continue...";
+            std::cin.ignore();
+            std::cin.get();
+            system(CLEAR_COMMAND.c_str()); // Use the clear screen command based on the detected OS
+        }
+    }
+    return 0;
 }
