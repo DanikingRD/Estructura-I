37eab5221b51c3a745577f94ad9ce29cbb81b304
danikingrd <danikingrd@gmail.com>
2023-09-19 15:50:29 -0400

    docs: add more comments

--- binary-tree/main.cpp
+++ binary-tree/main.cpp
@@ -13,214 +13,253 @@
 
 #include <iostream>
 
-#ifdef _WIN32 // Check if the code is running on a Windows system
-const std::string CLEAR_COMMAND = "cls"; // Windows clear screen command
+#ifdef _WIN32 // Detectar si esta corriendo en un sistema windows
+const std::string CLEAR_COMMAND = "cls"; // Usar el comando cls para limpiar la pantalla en Windows
 #else
-const std::string CLEAR_COMMAND = "clear"; // UNIX/Linux/Mac clear screen command
+const std::string CLEAR_COMMAND = "clear"; // Usar el comando clear para limpiar la pantalla en sistemas UNIX
 #endif
 
+// Definición de la estructura Node (Nodo)
 struct Node {
-    int value;
-    Node* parent;
-    Node* left;
-    Node* right;
+    int value;     // Valor almacenado en el nodo
+    Node* parent;  // Nodo padre
+    Node* left;    // Nodo hijo izquierdo
+    Node* right;   // Nodo hijo derecho
 
+    // Método constructor del nodo
     Node(Node* parent, int value) : parent(parent), value(value), left(nullptr), right(nullptr) {}
 };
 
+// Definición de la clase BinarySearchTree (Árbol Binario de Búsqueda)
 class BinarySearchTree {
 private:
-    Node* root;
+    Node* root; // Raíz del árbol
 
 public:
+    // Método constructor de la clase BinarySearchTree
     BinarySearchTree() : root(nullptr) {}
 
+    // Método destructor de la clase BinarySearchTree, responsable de liberar memoria
     ~BinarySearchTree() {
         destroyTree(root);
     }
-	
-	// Insertar un valor al árbol.
+
+    // Insertar un valor al árbol.
     void insert(int value) {
         insert(root, nullptr, value);
     }
-	
-	// Eliminar un valor del árbol.
+
+    // Eliminar un valor del árbol.
     bool remove(int value) {
         return remove(root, value);
     }
-	// Verifica si el árbol contiene un valor dado.
+
+    // Verificar si el árbol contiene un valor dado.
     bool contains(int value) const {
         return contains(root, value);
     }
-	// Imprime el árbol.
+
+    // Imprimir el árbol.
     void display() const {
         displayTree(root);
     }
-	// Recorre e imprime los valores del árbol de manera pre-order
+
+    // Recorrer e imprimir los valores del árbol en preorden.
     void printPreOrder() const {
         printPreOrder(root);
     }
-	// Recorre e imprime los valores del árbol de manera in-order
+
+    // Recorrer e imprimir los valores del árbol en orden.
     void printInOrder() const {
         printInOrder(root);
     }
-	
-	// Recorre e imprime los valores del árbol de manera post-order
+
+    // Recorrer e imprimir los valores del árbol en postorden.
     void printPostOrder() const {
         printPostOrder(root);
     }
-	
-	// Verifica si el árbol está vacío.
+
+    // Verificar si el árbol está vacío.
     bool isEmpty() const {
         return root == nullptr;
     }
 
 private:
     void insert(Node*& node, Node* parent, int value) {
-		if (node == nullptr) {
-			// Si el nodo es nulo creamos un nuevo nodo.
+        if (node == nullptr) {
+            // Si el nodo es nulo, creamos un nuevo nodo.
             node = new Node(parent, value);
             return;
         }
-		// Nos movemos hasta encontrar un nodo nulo
+        // Nos movemos hasta encontrar un nodo nulo.
         if (value < node->value) {
-			// Los valores menores al padre van a la izquierda.
+            // Los valores menores al padre van a la izquierda.
             insert(node->left, node, value);
         } else {
-			// Los valores mayores al padre van a la derecha.
+            // Los valores mayores al padre van a la derecha.
             insert(node->right, node, value);
         }
     }
 
     bool remove(Node*& node, int value) {
         if (node == nullptr) {
-			// Si el nodo es nulo el valor existe.
+            // Si el nodo es nulo, el valor no existe.
             return false;
         }
-		// Nos movemos en el arbol.
+        // Nos movemos en el árbol.
         if (value < node->value) {
-			// Nos movemos hacia la izquierda siempre que tengamos un valor menor
+            // Nos movemos hacia la izquierda siempre que tengamos un valor menor.
             return remove(node->left, value);
         } else if (value > node->value) {
-			// Nos movemos hasta la derecha siempre que tengamos un valor mayor.
+            // Nos movemos hacia la derecha siempre que tengamos un valor mayor.
             return remove(node->right, value);
         } else {
-			// Entrar aqui significa que node->value y value son iguales
-			// vamos a removerlo.
+            // Entrar aquí significa que node->value y value son iguales,
+            // vamos a removerlo.
             removeNode(node);
             return true;
         }
     }
-	
+
     void removeNode(Node*& node) {
-		// Caso 1) - El nodo tiene dos subárboles.
+        // Caso 1) - El nodo tiene dos subárboles.
         if (node->left && node->right) {
-			// 1. Encontramos el mínimo luego de habernos desplazado a la derecha.
+            // 1. Encontramos el mínimo luego de habernos desplazado a la derecha.
             Node* min = findMin(node->right);
-			// 2. Reemplazamos el valor del nodo padre con el nuevo valor.
+            // 2. Reemplazamos el valor del nodo padre con el nuevo valor.
             node->value = min->value;
-			// 3. Removemos el nodo a 
+            // 3. Removemos el nodo mínimo.
             remove(node->right, min->value);
         } else {
-			// Caso 2 y 3) - El nodo tiene un sub-árbol derecho o uno izquierdo.
+            // Caso 2 y 3) - El nodo tiene un subárbol derecho o uno izquierdo.
             Node* temp = node;
 
             if (node->left) {
-				// Si tiene un sub-arbol izquierdo hacemos que apunte
-				// a su hijo izquierdo.
+                // Si tiene un subárbol izquierdo, hacemos que apunte
+                // a su hijo izquierdo.
                 node = node->left;
             } else if (node->right) {
-				// Si tiene un sub-arbol derecho hacemos que apunte
-				// a su hijo derecho.
+                // Si tiene un subárbol derecho, hacemos que apunte
+                // a su hijo derecho.
                 node = node->right;
             } else {
-				// Si no tiene hijos lo eliminamos.
+                // Si no tiene hijos, lo eliminamos.
                 node = nullptr;
             }
             delete temp;
         }
     }
-	
-	// Encuentra el menor valor de un árbol
+
+    // Encuentra el menor valor de un árbol
     Node* findMin(Node* subTree) const {
         if (!subTree || !subTree->left) {
             return subTree;
         }
-		// Consiste en desplazarnos a la izquierda hasta
-		// que se evalúe en true la condición anterior.
-		// Lo cual significa que no hay más nodos la izquierda.
+        // Consiste en desplazarnos a la izquierda hasta
+        // que se evalúe en true la condición anterior,
+        // lo cual significa que no hay más nodos a la izquierda.
         return findMin(subTree->left);
     }
 
-
     bool contains(const Node* node, int value) const {
         if (node == nullptr) {
-			// Si el árbol es nulo no tenemos que verificar nada.
+            // Si el árbol es nulo, no tenemos que verificar nada.
             return false;
         }
-		// Si encontramos ún numero igual retornamos true.
+        // Si encontramos un número igual, retornamos true.
         if (node->value == value) {
             return true;
-			// Nos movemos a la izquierda tantas veces hasta encontrar
-			// un numero mayor o igual
         } else if (value < node->value) {
+            // Nos movemos a la izquierda tantas veces como sea necesario
+            // hasta encontrar un número mayor o igual.
             return contains(node->left, value);
         } else {
-			// Nos movemos a la derecha tantas veces hasta encontrar un numero
-			// mayor o igual.
+            // Nos movemos a la derecha tantas veces como sea necesario
+            // hasta encontrar un número mayor o igual.
             return contains(node->right, value);
         }
     }
 
+    // Función que muestra el árbol en forma visual con indentación.
+    // Utiliza un recorrido inverso en orden (derecha, raíz, izquierda) para imprimirlo de manera ordenada.
     void displayTree(const Node* node, int indent = 0) const {
         if (node == nullptr) {
-            return;
+            return; // Si el nodo es nulo, no hay nada que mostrar.
         }
+
+        // Primero, llamamos recursivamente a la función con el hijo derecho (mayor valor).
         displayTree(node->right, indent + 1);
+
+        // Luego, imprimimos espacios en blanco para la indentación.
         for (int level = 0; level < indent; level++) {
             std::cout << "   ";
         }
+
+        // Después, imprimimos el valor del nodo actual.
         std::cout << node->value << std::endl;
+
+        // Finalmente, llamamos recursivamente a la función con el hijo izquierdo (menor valor).
         displayTree(node->left, indent + 1);
     }
 
+    // Función que destruye un árbol liberando la memoria de todos los nodos.
     void destroyTree(Node* node) {
         if (node) {
+            // Si el nodo no es nulo, llamamos recursivamente a la función para los hijos izquierdo y derecho.
             destroyTree(node->left);
             destroyTree(node->right);
+            // Luego, liberamos la memoria del nodo actual.
             delete node;
         }
     }
 
+    // Función para imprimir el árbol en preorden (raíz, izquierda, derecha).
     void printPreOrder(const Node* node) const {
-        if (node == nullptr) return;
+        if (node == nullptr) return; // Si el nodo es nulo, no hacemos nada.
+
+        // Primero, imprimimos el valor del nodo actual.
         std::cout << node->value << " - ";
+
+        // Luego, llamamos recursivamente a la función para el hijo izquierdo y el hijo derecho.
         printPreOrder(node->left);
         printPreOrder(node->right);
     }
 
+    // Función para imprimir el árbol en orden (izquierda, raíz, derecha).
     void printInOrder(const Node* node) const {
-        if (node == nullptr) return;
+        if (node == nullptr) return; // Si el nodo es nulo, no hacemos nada.
+
+        // Primero, llamamos recursivamente a la función para el hijo izquierdo.
         printInOrder(node->left);
+
+        // Luego, imprimimos el valor del nodo actual.
         std::cout << node->value << std::endl;
+
+        // Finalmente, llamamos recursivamente a la función para el hijo derecho.
         printInOrder(node->right);
     }
 
+    // Función para imprimir el árbol en postorden (izquierda, derecha, raíz).
     void printPostOrder(const Node* node) const {
-        if (node == nullptr) return;
+        if (node == nullptr) return; // Si el nodo es nulo, no hacemos nada.
+
+        // Primero, llamamos recursivamente a la función para el hijo izquierdo y el hijo derecho.
         printPostOrder(node->left);
         printPostOrder(node->right);
+
+        // Luego, imprimimos el valor del nodo actual, seguido de un guión y una nueva línea.
         std::cout << node->value << " - " << std::endl;
     }
+
 };
 
 int main() {
-    std::cout << "Running Binary Search Tree program...\n";
+    std::cout << "Ejecutando programa de Árbol Binario de Búsqueda...\n";
     BinarySearchTree tree;
     bool exit = false;
 
     while (!exit) {
-        std::cout << "Press:\n"
+        std::cout << "Presiona:\n"
                   << "   0) - Salir del programa.\n"
                   << "   1) - Insertar un elemento en el árbol.\n"
                   << "   2) - Eliminar un elemento del árbol.\n"
@@ -229,7 +268,7 @@ int main() {
                   << "   5) - Imprimir recorrido InOrden.\n"
                   << "   6) - Imprimir recorrido PostOrden.\n"
                   << "   7) - Mostrar el árbol.\n"
-                  << "Enter your choice: ";
+                  << "Ingresa tu elección: ";
 
         int option;
         std::cin >> option;
@@ -261,7 +300,7 @@ int main() {
                     std::cout << "Ingresa el valor a revisar: ";
                     std::cin >> value;
                     if (tree.contains(value)) {
-                        std::cout << " * El valor " << value << " si está en el árbol.\n";
+                        std::cout << " * El valor " << value << " sí está en el árbol.\n";
                     } else {
                         std::cout << " * El valor " << value << " no está en el árbol.\n";
                     }
@@ -271,16 +310,16 @@ int main() {
                 if (tree.isEmpty()) {
                     std::cout << " * El árbol está vacío. No se puede realizar un recorrido.\n";
                 } else {
-                    std::cout << "Recorrido PreOrder: ";
+                    std::cout << "Recorrido PreOrden: ";
                     tree.printPreOrder();
                     std::cout << std::endl;
                 }
                 break;
             case 5:
                 if (tree.isEmpty()) {
-					std::cout << " * El árbol está vacío. No se puede realizar un recorrido.\n";
+                    std::cout << " * El árbol está vacío. No se puede realizar un recorrido.\n";
                 } else {
-                    std::cout << "Recorrido InOrder:\n";
+                    std::cout << "Recorrido InOrden:\n";
                     tree.printInOrder();
                 }
                 break;
@@ -288,7 +327,7 @@ int main() {
                 if (tree.isEmpty()) {
                     std::cout << " * El árbol está vacío. No se puede realizar un recorrido.\n";
                 } else {
-                    std::cout << "Recorrido PostOrder: ";
+                    std::cout << "Recorrido PostOrden: ";
                     tree.printPostOrder();
                     std::cout << std::endl;
                 }
@@ -297,12 +336,12 @@ int main() {
                 if (tree.isEmpty()) {
                     std::cout << " * El árbol está vacío. No hay nada que mostrar.\n";
                 } else {
-                    std::cout << "Mostrándo árbol:\n";
+                    std::cout << "Mostrando árbol:\n";
                     tree.display();
                 }
                 break;
             default:
-                std::cout << " * La opción " << option << " no existe. Intenta de nuevo.\n";
+                std::cout << " * La opción " << option << " no existe. Inténtalo de nuevo.\n";
                 break;
         }
 
@@ -310,7 +349,7 @@ int main() {
             std::cout << "Presiona Enter para continuar...";
             std::cin.ignore();
             std::cin.get();
-            system(CLEAR_COMMAND.c_str()); // Use the clear screen command based on the detected OS
+            system(CLEAR_COMMAND.c_str()); // Usar el comando clear o cls dependiendo del sistema operativo
         }
     }
     return 0;
