227180d71b8343aabe1402bda1a8440f1f1c257e
DanikingRD <danikingrd@gmail.com>
2023-10-03 19:16:43 -0400

    fix: date

--- torres-de-hanoi/main.cpp
+++ torres-de-hanoi/main.cpp
@@ -8,7 +8,7 @@
  *              1114950 - Elian Gabriel Villegas Báez
  *              1116614 - Luis Daniel de la Cruz García
  *              1116623 - Aquilenyi Suero de los Santos
- * FECHA: 17/09/2023 <== Fecha de realización
+ * FECHA: 3/10/2023 <== Fecha de realización
  */
 #include <iostream>
 
