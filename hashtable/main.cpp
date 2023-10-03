d8e9e5b739e26178cf3f4c593ecc9087859aabc0
danikingrd <danikingrd@gmail.com>
2023-10-03 11:25:17 -0400

    feat: add student ids

--- hashtable/main.cpp
+++ hashtable/main.cpp
@@ -8,19 +8,27 @@
  *              1114950 - Elian Gabriel Villegas Báez
  *              1116614 - Luis Daniel de la Cruz García
  *              1116623 - Aquilenyi Suero de los Santos
- * FECHA: 17/09/2023 <== Fecha de realización
+ * FECHA: 1/09/2023 <== Fecha de realización
  */
 #include <iomanip>
 #include <iostream>
-using namespace std;
 
-const unsigned int MAX = 40;
+using namespace std;
 
 struct Student;
 int readInt();
 string readStr();
-Student* createStudent(int studentId);
+Student* createStudent(int studentId, bool idOnly);
 void printTable(Student* current, int slot);
+
+int IDS[] = {1115075, 1116614, 1114790, 1113810, 1114116, 1115242, 1116238,
+             1113902, 1109094, 1114056, 1114814, 1113684, 1116176, 1087257,
+             1116783, 1115079, 1114306, 1116906, 1115419, 1114204, 1115989,
+             1115915, 1116733, 1114290, 1113744, 1114441, 1115997, 1114531,
+             1115279, 1115395, 1114042, 1114343, 1116842, 1105307, 1113891,
+             1116623, 1116351, 1114929, 1114331, 1114950};
+
+const unsigned int TOTAL_ID_COUNT = sizeof(IDS) / sizeof(IDS[0]);
 
 struct Student {
     int id;
@@ -30,73 +38,34 @@ struct Student {
     Student* next;
 };
 
-Student hash_table[MAX] = {};
+Student table[TOTAL_ID_COUNT] = {};
 
-void displayHashTable() {
-    cout << " * Imprimiendo tabla hash..." << endl;
-    for (int i = 0; i < MAX; i++) {
-        Student* current = &hash_table[i];
-        while (current) {
-            printTable(current, i);
-            current = current->next;
-        }
+bool arrayContains(int* arr, int size, int value) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == value)
+            return true;
     }
+    return false;
 }
 
-int hashfn(int id) { return id % MAX; }
+// Regresa un número entre 0 y MAX
+int hashFN(int id) { return id % TOTAL_ID_COUNT; };
 
-void insertStudent(int studentId) {
-    // check if table is full
-    if (hash_table[MAX - 1].id) {
-        cout << "Tabla llena" << endl;
-        return;
+Student* createStudent(int studentId, bool idOnly) {
+    if (idOnly) {
+        Student* student = new (Student){
+            .id = studentId,
+            .next = 0,
+        };
+        return student;
     }
-    int table_index = hashfn(studentId);
-    // check if there is a collision
-    if (hash_table[table_index].id) {
-        cout << "Resolviendo colisión..." << endl;
-        // resolve collision
-        Student* current = &hash_table[table_index];
-        // go to the last element
-        while (current->next) {
-            current = current->next;
-        }
-        Student* st = createStudent(studentId);
-        current->next = st;
-    } else {
-        // There is no collision
-        Student* st = createStudent(studentId);
-        hash_table[table_index] = *st;
-    }
-    cout << " * Estudiante insertado en slot #" << table_index << endl;
-}
-
-void get(int studentId) {
-    // Resumen de pasos:
-    // 1) Hashear el id del estudiante
-    // 2) Buscar el bucket correspondiente
-    // 3) Recorrer la lista enlazada hasta encontrar el estudiante
-    // 4) Imprimir el estudiante
-    cout << "Buscando estudiante con Id: " << studentId << endl;
-    int hash = hashfn(studentId);
-    Student* entry = &hash_table[hash];
-    while (entry) {
-        if (entry->id == studentId) {
-            printTable(entry, hash);
-            return;
-        }
-        entry = entry->next;
-    }
-    cout << "No se encontró el estudiante con id: " << studentId << endl;
-}   
-
-Student* createStudent(int studentId) {
     cout << "Ingrese el nombre del estudiante: ";
     string name = readStr();
     cout << "Ingrese el nombre del coordinador: ";
     string coordinator = readStr();
     cout << "Ingrese el indice académico del estudiante: ";
     int academic_idx = readInt();
+
     Student* student = new (Student){
         .id = studentId,
         .name = name,
@@ -107,18 +76,75 @@ Student* createStudent(int studentId) {
     return student;
 }
 
+void insertStudent(int studentId, bool idOnly = false) {
+    if (!arrayContains(IDS, TOTAL_ID_COUNT, studentId)) {
+        cout << "El estudiante con id: " << studentId
+             << " no está en el curso.\n";
+        return;
+    }
+    Student* st = 0;
+    int bucket = hashFN(studentId);
+
+    if (table[bucket].id) {
+        // resolver colisión
+        Student* current = &table[bucket];
+        while (current->next) {
+            current = current->next;
+        }
+        st = createStudent(studentId, idOnly);
+        current->next = st;
+    } else {
+        // insertar en bucket, no hay colisión
+        // será la cabeza de la lista enlazada
+        st = createStudent(studentId, idOnly);
+        table[bucket] = *st;
+    }
+    cout << " * Estudiante insertado en slot #" << bucket << endl;
+}
+
+void populate() {
+    for (int i = 0; i < TOTAL_ID_COUNT; i++) {
+        int studentId = IDS[i];
+        int index = hashFN(i);
+        insertStudent(studentId, true);
+    }
+}
+
+void displayHashTable() {
+    cout << " * Imprimiendo tabla hash..." << endl;
+    for (int i = 0; i < TOTAL_ID_COUNT; i++) {
+        Student* current = &table[i];
+        while (current) {
+            printTable(current, i);
+            current = current->next;
+        }
+    }
+}
+
+void searchStudent(int studentId) {
+    // Resumen de pasos:
+    // 1) Hashear el id del estudiante
+    // 2) Buscar el bucket correspondiente
+    // 3) Recorrer la lista enlazada hasta encontrar el estudiante
+    // 4) Imprimir el estudiante
+    cout << "Buscando estudiante con Id: " << studentId << endl;
+    int bucket = hashFN(studentId);
+    Student* st = &table[bucket];
+    while (st) {
+        if (st->id == studentId) {
+            printTable(st, bucket);
+            return;
+        }
+        st = st->next;
+    }
+    cout << "No se encontró el estudiante con id: " << studentId << endl;
+}
+
 string padString(string str, int width) {
     return string(width - str.length(), ' ') + str;
 }
 
 void printTable(Student* current, int slot) {
-    string id_pad = padString(to_string(current->id), 35);
-    string name_padded = padString(current->name, 31);
-    string coordinator_padded = padString(current->coordinator, 26);
-    string academic_idx_padded =
-        padString(to_string(current->academic_idx), 21);
-    string nextPtr = current->next ? to_string(current->next->id) : "null";
-    string next_padded = padString(nextPtr, 28);
 
     if (slot > 9)
         cout << "┌─────────────────"
@@ -126,11 +152,15 @@ void printTable(Student* current, int slot) {
     else
         cout << "┌──────────────────"
              << " Slot#" << slot << " ──────────────┐  " << endl;
-    cout << "│ Id: " << id_pad << "│ " << endl;
-    cout << "│ Nombre: " << name_padded << "│ " << endl;
-    cout << "│ Coordinador: " << coordinator_padded << "│ " << endl;
-    cout << "│ Indice académico: " << academic_idx_padded << "│" << endl;
-    cout << "│ Siguiente: " << next_padded << "│" << endl;
+
+    string nextPtr = current->next ? to_string(current->next->id) : "null";
+    cout << "│ Id: " << padString(to_string(current->id), 35) << "│ " << endl;
+    cout << "│ Nombre: " << padString(current->name, 31) << "│ " << endl;
+    cout << "│ Coordinador: " << padString(current->coordinator, 26) << "│ "
+         << endl;
+    cout << "│ Indice académico: "
+         << padString(to_string(current->academic_idx), 21) << "│" << endl;
+    cout << "│ Siguiente: " << padString(nextPtr, 28) << "│" << endl;
     cout << "└────────────────────────────────────────┘" << endl;
 }
 
@@ -157,11 +187,12 @@ int readInt() {
 }
 
 void run() {
+    populate();
     displayHashTable();
     while (true) {
         cout << "Seleccione una opción: " << endl;
         cout << "0. Salir" << endl;
-        cout << "1. Insertar un estudiante." << endl;
+        cout << "1. Insertar campos de un estudiante." << endl;
         cout << "2. Buscar un estudiante." << endl;
         cout << "3. Para mostrar la tabla hash." << endl;
         cout << "Opción: ";
@@ -176,18 +207,12 @@ void run() {
             cout << "Ingrese el id del estudiante: ";
             int studentId;
             studentId = readInt();
-            cout << "Insertando estudiante con Id: " << studentId << endl;
             insertStudent(studentId);
             break;
         case 2:
-            //check if empty
-            if (!hash_table[0].id) {
-                cout << " * Tabla vacía. Inserte un estudiante primero." << endl;
-                break;
-            }
             cout << "Ingrese el id del estudiante: ";
             studentId = readInt();
-            get(studentId);
+            searchStudent(studentId);
             break;
         case 3:
             displayHashTable();
@@ -195,25 +220,23 @@ void run() {
         default:
             break;
         }
-        cout << endl;
-
         if (exit) {
             cout << "Saliendo del programa...\n";
+            cout << "Liberando memoria...\n";
             // free memory
-            for (int i = 0; i < MAX; i++) {
-                Student* bucket = &hash_table[i];
-                // delete all linked lists in the bucket
-                while (bucket && bucket->id) {
-                    Student* next = bucket->next;
-                    delete bucket;
-                    bucket = next;
+            for (int i = 0; i < TOTAL_ID_COUNT; i++) {
+                Student* current = table[i].next; 
+                while (current) {
+                    Student* temp = current; 
+                    current = current->next; 
+                    delete temp;             
                 }
-        
             }
             break;
         }
+
+        cout << endl;
     }
-    
 }
 
 int main(void) {
